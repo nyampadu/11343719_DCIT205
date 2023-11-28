d84230f0a38515b805d6c5a8b63b587478a4001b
nyampadu <151385393+nyampadu@users.noreply.github.com>
2023-11-27 12:19:20 -1200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+document.getElementById('facultyBtn').addEventListener('click', function () {
+    window.location.href = 'faculty.html';
+});
+
+document.getElementById('eventsBtn').addEventListener('click', function () {
+    window.location.href = 'event.html';
+});
+
+document.getElementById('coursesBtn').addEventListener('click', function () {
+    window.location.href = 'courses.html';
+});

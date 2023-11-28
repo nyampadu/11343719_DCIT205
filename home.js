8308ba7e5c76339c2adf3b7b4e2eb0ea540b07f5
nyampadu <151385393+nyampadu@users.noreply.github.com>
2023-11-28 00:49:14 -1200

    Add files via upload

--- home.js
+++ home.js
@@ -9,3 +9,20 @@ document.getElementById('eventsBtn').addEventListener('click', function () {
 document.getElementById('coursesBtn').addEventListener('click', function () {
     window.location.href = 'courses.html';
 });
+
+
+const footer = document.querySelector('footer');
+const slideTrigger = window.innerHeight * (2 / 3);
+
+function updateFooterStyle() {
+    if (window.scrollY > slideTrigger) {
+        footer.classList.add('slide-up');
+    } else {
+        footer.classList.remove('slide-up');
+    }
+}
+
+window.addEventListener('scroll', updateFooterStyle);
+
+updateFooterStyle();
+
